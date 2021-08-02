31182746af665ec932d113748d66e7b6b34ba60f
zwz9599 <792438443@qq.com>
2021-08-02 17:13:16 +0800

    ip check

---
+++ ip_check.sh
@@ -0,0 +1,24 @@
+#! /bin/bash 
+
+#判断传入参数的个数，如果参数个数不等于1，程序退出
+if [ $# -ne 1 ]; then
+ echo "参数错误"
+ exit 1
+else
+  
+line=$1 #变量1
+
+for ip  in  $(cat $line )
+do 
+ echo "$ip"
+ ping -c 1 $ip > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+   echo "good"
+  else 
+   echo "bad"
+  fi
+   
+done
+fi
+
+
